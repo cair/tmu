bbb7f6fec9318d4352c26499761f888c6ed169cc
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-11-28 19:57:51 +0100

    Added convolution

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -28,11 +28,12 @@ import numpy as np
 from tmu.clause_bank import ClauseBank
 
 class TMClassifier():
-	def __init__(self, number_of_clauses, T, s, boost_true_positive_feedback=1, number_of_state_bits=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
+	def __init__(self, number_of_clauses, T, s, patch_dim=None, boost_true_positive_feedback=1, number_of_state_bits=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
 		self.number_of_clauses = number_of_clauses
 		self.number_of_state_bits = number_of_state_bits
 		self.T = int(T)
 		self.s = s
+		self.patch_dim = patch_dim
 		self.boost_true_positive_feedback = boost_true_positive_feedback
 		self.weighted_clauses = weighted_clauses
 
@@ -45,9 +46,26 @@ class TMClassifier():
 		if self.initialize == True:
 			self.initialize = False
 			self.number_of_classes = int(np.max(Y) + 1)
-			self.number_of_literals = X.shape[1]*2
+
+			self.dim_x = X.shape[1]
+			if len(X.shape) == 2:
+				self.dim_y = 1
+				self.dim_z = 1
+			elif len(X.shape) == 3:
+				self.dim_y = X.shape[2]
+				self.dim_z = 1
+			elif len(X.shape) == 4:
+				self.dim_z = X.shape[3]
+
+			if self.patch_dim == None:
+				self.patch_dim = (X.shape[1], 1)
+
+			self.number_of_features = int(self.patch_dim[0]*self.patch_dim[1]*self.dim_z + (self.dim_x - self.patch_dim[0]) + (self.dim_y - self.patch_dim[1]))
+			self.number_of_literals = self.number_of_features*2
+			
+			self.number_of_patches = int((self.dim_x - self.patch_dim[0] + 1)*(self.dim_y - self.patch_dim[1] + 1))
 			self.number_of_ta_chunks = int((self.number_of_literals-1)/32 + 1)
-			self.number_of_patches = 1
+
 			self.clause_banks = []
 			for i in range(self.number_of_classes):
 				self.clause_banks.append((ClauseBank(self.number_of_clauses//2, self.number_of_literals, self.number_of_state_bits, self.number_of_patches, self.weighted_clauses), ClauseBank(self.number_of_clauses//2, self.number_of_literals, self.number_of_state_bits, self.number_of_patches, self.weighted_clauses)))
@@ -56,7 +74,7 @@ class TMClassifier():
 				self.clause_banks[i][0].initialize_clauses()
 				self.clause_banks[i][1].initialize_clauses()
 
-		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_ta_chunks, self.number_of_literals//2, 1, 1, self.number_of_literals//2, 1, 0)
+		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_patches, self.number_of_ta_chunks, self.dim_x, self.dim_y, self.dim_z, self.patch_dim[0], self.patch_dim[1], 0)
 		Ym = np.ascontiguousarray(Y).astype(np.uint32)
 		
 		clause_active = []
@@ -89,8 +107,8 @@ class TMClassifier():
 			self.clause_banks[not_target][0].cb_type_ii_feedback(update_p, clause_active[not_target][0], encoded_X[e,:])			
 		return
 
-	def predict(self, X):		
-		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_ta_chunks, self.number_of_literals//2, 1, 1, self.number_of_literals//2, 1, 0)	
+	def predict(self, X):
+		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_patches, self.number_of_ta_chunks, self.dim_x, self.dim_y, self.dim_z, self.patch_dim[0], self.patch_dim[1], 0)
 		Y = np.ascontiguousarray(np.zeros(X.shape[0], dtype=np.uint32))
 		for e in range(X.shape[0]):
 			max_class_sum = -self.T
