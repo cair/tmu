7c8f287eba3587255156ce02d3390b2dc2ce8faa
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-11-29 20:28:03 +0100

    Added Coalesced TM

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -138,3 +138,100 @@ class TMClassifier():
 		polarity = clause >= self.number_of_clauses//2
 		clause_pos = clause % (self.number_of_clauses//2)
 		return self.clause_banks[data_class][polarity].ta_action(clause_pos, ta)
+
+class TMCoalescedClassifier():
+	def __init__(self, number_of_clauses, T, s, patch_dim=None, boost_true_positive_feedback=1, number_of_state_bits=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
+		self.number_of_clauses = number_of_clauses
+		self.number_of_state_bits = number_of_state_bits
+		self.T = int(T)
+		self.s = s
+		self.patch_dim = patch_dim
+		self.boost_true_positive_feedback = boost_true_positive_feedback
+		self.weighted_clauses = weighted_clauses
+
+		self.clause_drop_p = clause_drop_p
+		self.literal_drop_p = literal_drop_p
+
+		self.initialized = False
+
+	def initialize(self, X, Y):
+		self.number_of_classes = int(np.max(Y) + 1)
+
+		if len(X.shape) == 2:
+			self.dim = (X.shape[1], 1 ,1)
+		elif len(X.shape) == 3:
+			self.dim = (X.shape[1], X.shape[2], 1)
+		elif len(X.shape) == 4:
+			self.dim = (X.shape[1], X.shape[2], X.shape[3])
+
+		if self.patch_dim == None:
+			self.patch_dim = (X.shape[1], 1)
+
+		self.number_of_features = int(self.patch_dim[0]*self.patch_dim[1]*self.dim[2] + (self.dim[0] - self.patch_dim[0]) + (self.dim[1] - self.patch_dim[1]))
+		self.number_of_literals = self.number_of_features*2
+		
+		self.number_of_patches = int((self.dim[0] - self.patch_dim[0] + 1)*(self.dim[1] - self.patch_dim[1] + 1))
+		self.number_of_ta_chunks = int((self.number_of_literals-1)/32 + 1)
+
+		self.clause_bank = ClauseBank(self.number_of_clauses, self.number_of_literals, self.number_of_state_bits, self.number_of_patches)
+		self.weight_banks = []
+		for i in range(self.number_of_classes):
+			self.weight_banks.append(WeightBank(np.ones(self.number_of_clauses).astype(np.int32)))
+		
+	def fit(self, X, Y, incremental=False):
+		if self.initialized == False or incremental == False:
+			self.initialize(X, Y)
+			self.initialized = True
+
+		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_patches, self.number_of_ta_chunks, self.dim, self.patch_dim, 0)
+		Ym = np.ascontiguousarray(Y).astype(np.uint32)
+		
+		clause_active = np.ascontiguousarray(np.random.choice(2, self.number_of_clauses, p=[self.clause_drop_p, 1.0 - self.clause_drop_p]).astype(np.int32))
+
+		for e in range(X.shape[0]):
+			target = Ym[e]
+
+			clause_outputs = self.clause_bank.calculate_clause_outputs_update(encoded_X[e,:])
+			class_sum = np.dot(clause_active * self.weight_banks[target].get_weights(), clause_outputs).astype(np.int32)
+			class_sum = np.clip(class_sum, -self.T, self.T)
+
+			update_p = (self.T - class_sum)/(2*self.T)
+
+			self.clause_bank.type_i_feedback(update_p, self.s, self.boost_true_positive_feedback, clause_active*(self.weight_banks[target].get_weights() >= 0), encoded_X[e,:])
+			self.clause_bank.type_ii_feedback(update_p, clause_active*(self.weight_banks[target].get_weights() < 0), encoded_X[e,:])
+
+			self.weight_banks[target].type_i_feedback(clause_outputs, update_p, clause_active)
+
+			not_target = np.random.randint(self.number_of_classes)
+			while not_target == target:
+				not_target = np.random.randint(self.number_of_classes)
+
+			clause_outputs = self.clause_bank.calculate_clause_outputs_update(encoded_X[e,:])
+			class_sum = np.dot(clause_active * self.weight_banks[not_target].get_weights(), clause_outputs).astype(np.int32)
+			class_sum = np.clip(class_sum, -self.T, self.T)
+			
+			update_p = (self.T + class_sum)/(2*self.T)
+		
+			self.clause_bank.type_i_feedback(update_p, self.s, self.boost_true_positive_feedback, clause_active * (self.weight_banks[not_target].get_weights() < 0), encoded_X[e,:])
+			self.clause_bank.type_ii_feedback(update_p, clause_active*(self.weight_banks[not_target].get_weights() >= 0), encoded_X[e,:])
+
+			self.weight_banks[not_target].type_ii_feedback(clause_outputs, update_p, clause_active, True)
+		return
+
+	def predict(self, X):
+		encoded_X = tmu.tools.encode(X, X.shape[0], self.number_of_patches, self.number_of_ta_chunks, self.dim, self.patch_dim, 0)
+		Y = np.ascontiguousarray(np.zeros(X.shape[0], dtype=np.uint32))
+		for e in range(X.shape[0]):
+			max_class_sum = -self.T
+			max_class = 0
+			for i in range(self.number_of_classes):
+				class_sum = np.dot(self.weight_banks[i].get_weights(), self.clause_bank.calculate_clause_outputs_update(encoded_X[e,:])).astype(np.int32)
+				class_sum = np.clip(class_sum, -self.T, self.T)
+				if class_sum > max_class_sum:
+					max_class_sum = class_sum
+					max_class = i
+			Y[e] = max_class
+		return Y
+
+	def ta_action(self, clause, ta):
+		return self.clause_bank.ta_action(clause, ta)
