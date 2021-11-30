fc8b7f30a369345389c615b0eaf4cfd1886eb4cb
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-11-30 23:17:40 +0100

    Update

---
+++ examples/MNISTDemo2DConvolutionOneVsOne.py
@@ -0,0 +1,23 @@
+import numpy as np
+from time import time
+
+from keras.datasets import mnist
+
+from tmu.tsetlin_machine import TMOneVsOneClassifier
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+X_train = np.where(X_train >= 75, 1, 0) 
+X_test = np.where(X_test >= 75, 1, 0) 
+
+tm = TMClassifier(20000, 50*100, 5.0, patch_dim=(10, 10), weighted_clauses=True)
+
+print("\nAccuracy over 30 epochs:\n")
+for i in range(30):
+	start = time()
+	tm.fit(X_train, Y_train, incremental=True)
+	stop = time()
+	
+	result = 100*(tm.predict(X_test) == Y_test).mean()
+	
+	print("#%d Accuracy: %.2f%% (%.2fs)" % (i+1, result, stop-start))
