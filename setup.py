798e6f4ca0c1f6f74399c9e1c9bab9041d40a1a3
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-11-30 23:00:53 +0100

    Added One vs One Multiclass Classifier

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.dirname(sys.argv[0]) or ".")
 
 setup(
     name='tmu',
-    version='0.1.8',
+    version='0.2.0',
     url='https://github.com/cair/tmu/',
     author='Ole-Christoffer Granmo',
     author_email='ole.granmo@uia.no',
