42b74ebbf61eec5930042a4947935afdfa7aba4e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-04 16:35:03 +0100

    Update

--- examples/MNISTClauseCoOccurrenceDemo.py
+++ examples/MNISTClauseCoOccurrenceDemo.py
@@ -3,7 +3,7 @@ from time import time
 
 from keras.datasets import mnist
 
-from tmu.tsetlin_machine import TMOneVsOneClassifier
+from tmu.tsetlin_machine import TMCoalescedClassifier
 
 np.set_printoptions(threshold=np.inf, linewidth=250, precision=2, suppress=True)
 
@@ -30,13 +30,18 @@ for z in range(resolution):
 X_train = X_train.reshape((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], resolution))
 X_test = X_test.reshape((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], resolution))
 
-tm = TMOneVsOneClassifier(clauses, T, s, patch_dim=(3, 3), weighted_clauses=True)
+tm = TMCoalescedClassifier(clauses, T, s, patch_dim=(3, 3), weighted_clauses=True)
 
-print("\nAccuracy over 10 epochs:\n")
-for i in range(10):
+print("\nAccuracy over 250 epochs:\n")
+for i in range(250):
 	start_training = time()
 	tm.fit(X_train, Y_train)
 	stop_training = time()
+
+	print(tm.clause_co_occurrence(X_test, percentage=True).toarray())
+
+	print(tm.clause_precision(0, True, X_test, Y_test))
+	print(tm.clause_recall(0, True, X_test, Y_test))
 
 	start_testing = time()
 	result = 100*(tm.predict(X_test) == Y_test).mean()
