2ec6e856ae2b4fc092e6adfeea70d36749ffb282
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-05 13:06:42 +0100

    Update

--- examples/InterpretabilityDemoOneVsOne.py
+++ examples/InterpretabilityDemoOneVsOne.py
@@ -26,9 +26,6 @@ for i in range(2):
 	precision.append((tm.clause_precision(i, 0, X_test, Y_test), tm.clause_precision(i, 1, X_test, Y_test)))
 	recall.append((tm.clause_recall(i, 0, X_test, Y_test), tm.clause_recall(i, 1, X_test, Y_test)))
 
-print(precision)
-print(recall)
-
 print("Accuracy:", 100*(tm.predict(X_test) == Y_test).mean())
 
 print("\nClauses:\n")
@@ -44,7 +41,9 @@ for j in range(clauses):
 	print(" ∧ ".join(l))
 
 	for i in range(2):
-		print("\tC:%d W:%+2d P:%.2f R:%.2f" % (i, tm.get_weight(i, j), precision[i][int(tm.get_weight(i, j) < 0)][j], recall[i][int(tm.get_weight(i, j) < 0)][j]))
+		print(precision[i][int(tm.get_weight(i, j) < 0)][0])
+		print(recall[i][int(tm.get_weight(i, j) < 0)][0])
+		print("\tC:%d W:%+2d" % (i, tm.get_weight(i, j)), "P:", precision[i][int(tm.get_weight(i, j) < 0)][0][j], "R:", recall[i][int(tm.get_weight(i, j) < 0)][0][j])
 
 print("\nClause Co-Occurence Matrix:\n")
 print(tm.clause_co_occurrence(X_test, percentage=True).toarray())
