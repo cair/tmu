f04b7cf5c4d9bf0680c0a4ce71bbfdc72e9363cd
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-06 00:31:49 +0100

    Update

--- examples/MNISTDemoWeightedClauses.py
+++ examples/MNISTDemoWeightedClauses.py
@@ -10,7 +10,7 @@ from tmu.tsetlin_machine import TMClassifier
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-tm = TMClassifier(20000, 50*100, 10.0, platform='CPU', weighted_clauses=True)
+tm = TMClassifier(2000, 50*100, 10.0, platform='CPU', weighted_clauses=True)
 
 print("\nAccuracy over 60 epochs:\n")
 for i in range(60):
