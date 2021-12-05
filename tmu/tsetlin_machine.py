e636ba719b3376cff50e6b605b6e413ba462c4af
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-06 00:22:50 +0100

    Update

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -169,7 +169,7 @@ class TMClassifier(TMBasis):
 			update_p = (self.T + class_sum)/(2*self.T)
 		
 			if self.weighted_clauses:
-				self.weight_banks[not_target].decrement(positive_clause_outputs, update_p, clause_active[not_target], False)			
+				self.weight_banks[not_target].decrement(clause_outputs, update_p, clause_active[not_target], False)			
 
 			if self.platform == 'CUDA':
 				self.clause_banks[not_target].type_i_feedback(update_p, self.s, self.boost_true_positive_feedback, clause_active[not_target]*self.negative_clauses, e)
