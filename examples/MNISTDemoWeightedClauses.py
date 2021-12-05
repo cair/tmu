99ac5b3e36767f8a13f05eeed3531579c3af0a1a
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-06 00:09:42 +0100

    Update

--- examples/MNISTDemoWeightedClauses.py
+++ examples/MNISTDemoWeightedClauses.py
@@ -3,14 +3,14 @@ from time import time
 
 from keras.datasets import mnist
 
-from tmu.tsetlin_machine import TMOneVsOneClassifier
+from tmu.tsetlin_machine import TMClassifier
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-tm = TMOneVsOneClassifier(20000, 50*100, 10.0, platform='CUDA', weighted_clauses=True)
+tm = TMClassifier(20000, 50*100, 10.0, platform='CUDA', weighted_clauses=True)
 
 print("\nAccuracy over 60 epochs:\n")
 for i in range(60):
