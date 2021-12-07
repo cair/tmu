d98fecf6240216616d21b14a8b74ae5775736013
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-07 19:56:07 +0100

    Update

--- examples/MNISTDemo2DConvolutionWeightedClauses.py
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -3,14 +3,14 @@ from time import time
 
 from keras.datasets import mnist
 
-from tmu.tsetlin_machine import TMCoalescedClassifier
+from tmu.tsetlin_machine import TMOneVsOneClassifier
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 X_train = np.where(X_train >= 75, 1, 0) 
 X_test = np.where(X_test >= 75, 1, 0) 
 
-tm = TMCoalescedClassifier(2500, int(250*0.75), 5.0, platform='CPU', patch_dim=(10, 10), weighted_clauses=True)
+tm = TMOneVsOneClassifier(2500, int(250*0.75), 5.0, platform='CPU', patch_dim=(10, 10), weighted_clauses=True)
 
 print("\nAccuracy over 250 epochs:\n")
 for i in range(250):
