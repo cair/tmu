0fcfdea6b4157e52546ae452a074427c6cb70f90
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-07 19:36:54 +0100

    Update

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -480,8 +480,8 @@ class TMOneVsOneClassifier(TMBasis):
 		self.weight_banks[output].get_weights()[output] = weight
 
 class TMRegressor(TMBasis):
-	def __init__(self, number_of_clauses, T, s, patch_dim=None, boost_true_positive_feedback=1, number_of_state_bits=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
-		super().__init__(number_of_clauses, T, s, patch_dim=patch_dim, boost_true_positive_feedback=boost_true_positive_feedback, number_of_state_bits=number_of_state_bits, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
+	def __init__(self, number_of_clauses, T, s, platform='CPU', patch_dim=None, boost_true_positive_feedback=1, number_of_state_bits=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
+		super().__init__(number_of_clauses, T, s, platform=platform, patch_dim=patch_dim, boost_true_positive_feedback=boost_true_positive_feedback, number_of_state_bits=number_of_state_bits, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
 
 	def initialize(self, X, Y):
 		if self.initialized == False:
