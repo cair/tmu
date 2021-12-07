ed3365bc4c5738f38977a9220b76270493a8dc6b
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-07 17:22:38 +0100

    Update

--- tmu/clause_bank_cuda_kernels.py
+++ tmu/clause_bank_cuda_kernels.py
@@ -127,7 +127,7 @@ code_clause_feedback = """
 		}
 
 		/* Calculate the output of each clause using the actions of each Tsetline Automaton. */
-		__device__ inline void calculate_clause_output_feedback(curandState *localState, unsigned int *ta_state, unsigned int *clause_output, unsigned int *clause_patch, int number_of_ta_chunks, int number_of_state_bits, unsigned int filter, unsigned int *Xi, unsigned int random_integer)
+		__device__ inline void calculate_clause_output_feedback(curandState *localState, unsigned int *ta_state, unsigned int *clause_output, unsigned int *clause_patch, int number_of_ta_chunks, int number_of_state_bits, unsigned int filter, unsigned int *Xi)
 		{	
 			unsigned int output_one_patches[NUMBER_OF_PATCHES];
 
@@ -156,7 +156,7 @@ code_clause_feedback = """
 
 			if (output_one_patches_count > 0) {
 				*clause_output = 1;
-				int patch_id = random_integer % output_one_patches_count;
+				int patch_id = curand(&localState) % output_one_patches_count;
 		 		*clause_patch = output_one_patches[patch_id];
 			} else {
 				*clause_output = 0;
@@ -251,7 +251,7 @@ code_clause_feedback = """
 
 				unsigned int clause_output;
 				unsigned int clause_patch;
-				calculate_clause_output_feedback(&localState, &ta_state[clause_pos], &clause_output, &clause_patch, number_of_ta_chunks, number_of_state_bits, filter, Xi, random_integers[j]);
+				calculate_clause_output_feedback(&localState, &ta_state[clause_pos], &clause_output, &clause_patch, number_of_ta_chunks, number_of_state_bits, filter, Xi);
 
 				if (clause_output) {				
 					for (int k = 0; k < number_of_ta_chunks; ++k) {
