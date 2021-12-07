3cfdf87c7a7d4075f2426504784f2e0b0141d458
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-07 20:17:18 +0100

    Update

--- examples/InterpretabilityDemoOneVsOne.py
+++ examples/InterpretabilityDemoOneVsOne.py
@@ -15,7 +15,7 @@ Y_train = np.where(np.random.rand(5000) <= noise, 1-Y_train, Y_train) # Adds noi
 X_test = np.random.randint(0, 2, size=(5000, number_of_features), dtype=np.uint32)
 Y_test = np.logical_xor(X_test[:,0], X_test[:,1]).astype(dtype=np.uint32)
 
-tm = TMOneVsOneClassifier(clauses, T, s, boost_true_positive_feedback=0)
+tm = TMOneVsOneClassifier(clauses, T, s, platform='CPU', boost_true_positive_feedback=0)
 
 for i in range(20):
 	tm.fit(X_train, Y_train)
