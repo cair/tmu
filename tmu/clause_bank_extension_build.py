f8e9c6559498cbb905e802c4852933f76154e089
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-12-11 21:44:14 +0100

    Update clause_bank_extension_build.py

--- tmu/clause_bank_extension_build.py
+++ tmu/clause_bank_extension_build.py
@@ -38,8 +38,7 @@ ffibuilder.set_source("tmu._cb",  # name of the output C extension
     #include "./tmu/ClauseBank.h"
 """,
     include_dirs=['.'],
-    sources=['./tmu/ClauseBank.c'],   # includes pi.c as additional sources
-    libraries=['m'])    # on Unix, link with the math library
+    sources=['./tmu/ClauseBank.c']) 
 
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
