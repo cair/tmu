205dd723cedbade8f779b080b2c74a9591485ca8
Ole-Christoffer Granmo <ole.granmo@uia.no>
2022-02-22 21:31:38 +0100

    Refactoring

--- examples/MNISTDemoCoalescedTypeIIIFeedback.py
+++ examples/MNISTDemoCoalescedTypeIIIFeedback.py
@@ -10,12 +10,12 @@ from tmu.tsetlin_machine import TMCoalescedClassifier
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-tm = TMCoalescedClassifier(20000, 50*100, 10.0, weighted_clauses=True)
+tm = TMCoalescedClassifier(20000, 50*100, 10.0, weighted_clauses=True, type_iii_feedback=True)
 
 print("\nAccuracy over 60 epochs:\n")
 for i in range(60):
         start_training = time()
-        tm.fit(X_train, Y_train, type_iii_feedback=True)
+        tm.fit(X_train, Y_train)
         stop_training = time()
 
         start_testing = time()
