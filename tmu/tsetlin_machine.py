da887250eecdbba1d8d03e1c895d637a0c55cd2d
Ole-Christoffer Granmo <ole.granmo@uia.no>
2022-06-09 15:38:50 +0200

    Multitask classifier

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -484,6 +484,152 @@ class TMCoalescedClassifier(TMBasis):
 	def set_weight(self, the_class, clause, weight):
 		self.weight_banks[the_class].get_weights()[clause] = weight
 
+
+class TMMultiTaskClassifier(TMBasis):
+	def __init__(self, number_of_clauses, T, s, type_iii_feedback=False, focused_negative_sampling=False, output_balancing=False, d=200.0, platform = 'CPU', patch_dim=None, feature_negation=True, boost_true_positive_feedback=1, number_of_state_bits_ta=8, number_of_state_bits_ind=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
+		super().__init__(number_of_clauses, T, s, type_iii_feedback=type_iii_feedback, focused_negative_sampling=focused_negative_sampling, output_balancing=output_balancing, d=d, platform = platform, patch_dim=patch_dim, feature_negation=feature_negation, boost_true_positive_feedback=boost_true_positive_feedback, number_of_state_bits_ta=number_of_state_bits_ta, number_of_state_bits_ind=number_of_state_bits_ind, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
+
+	def initialize(self, X, Y):
+		self.number_of_classes = X.shape[0]
+	
+		if self.platform == 'CPU':
+			self.clause_bank = ClauseBank(X[0], self.number_of_clauses, self.number_of_state_bits_ta, self.number_of_state_bits_ind, self.patch_dim)
+		elif self.platform == 'CUDA':
+			from tmu.clause_bank_cuda import ClauseBankCUDA
+			self.clause_bank = ClauseBankCUDA(X[0], self.number_of_clauses, self.number_of_state_bits_ta, self.patch_dim)
+		else:
+			print("Unknown Platform")
+			sys.exit(-1)
+
+		self.weight_banks = []
+		for i in range(self.number_of_classes):
+			self.weight_banks.append(WeightBank(np.random.choice([-1,1], size=self.number_of_clauses).astype(np.int32)))
+			
+	def fit(self, X, Y, shuffle=True):
+		if self.initialized == False:
+			self.initialize(X, Y)
+			self.initialized = True
+
+		if not np.array_equal(self.X_train, X):
+			self.encoded_X_train = {}
+			for i in range(self.number_of_classes):
+				self.encoded_X_train[i] = self.clause_bank.prepare_X(X[i])
+			self.X_train = X.copy()
+
+		Ym = np.ascontiguousarray(Y).astype(np.uint32)
+
+		# Clauses are dropped based on their weights
+		self.clause_active = np.ones(self.number_of_clauses, dtype=np.uint32)
+		clause_score = np.zeros(self.number_of_clauses, dtype=np.int32)
+		for i in range(self.number_of_classes):
+			clause_score += np.abs(self.weight_banks[i].get_weights())
+		deactivate = np.random.choice(np.arange(self.number_of_clauses), size=int(self.number_of_clauses*self.clause_drop_p), p = clause_score / clause_score.sum())
+		for d in range(deactivate.shape[0]):
+			self.clause_active[deactivate[d]] = 0
+
+		# Literals are dropped based on their frequency
+		self.literal_active = (np.zeros(self.clause_bank.number_of_ta_chunks, dtype=np.uint32) | ~0).astype(np.uint32)
+		literal_clause_frequency = self.literal_clause_frequency()
+		if literal_clause_frequency.sum() > 0:
+			deactivate = np.random.choice(np.arange(self.clause_bank.number_of_literals), size=int(self.clause_bank.number_of_literals*self.literal_drop_p), p = literal_clause_frequency / literal_clause_frequency.sum())
+		else:
+			deactivate = np.random.choice(np.arange(self.clause_bank.number_of_literals), size=int(self.clause_bank.number_of_literals*self.literal_drop_p))
+		for d in range(deactivate.shape[0]):
+			ta_chunk = deactivate[d] // 32
+			chunk_pos = deactivate[d] % 32
+			self.literal_active[ta_chunk] &= (~(1 << chunk_pos))
+
+		if not self.feature_negation:
+			for k in range(self.clause_bank.number_of_literals//2, self.clause_bank.number_of_literals):
+				ta_chunk = k // 32
+				chunk_pos = k % 32
+				self.literal_active[ta_chunk] &= (~(1 << chunk_pos))
+
+		self.literal_active = self.literal_active.astype(np.uint32)
+
+		shuffled_index = np.arange(X[0].shape[0])
+		if shuffle:
+			np.random.shuffle(shuffled_index)
+
+		for e in shuffled_index:
+			for i in range(self.number_of_classes):
+				clause_outputs = self.clause_bank.calculate_clause_outputs_update(self.literal_active, self.encoded_X_train[i], e)
+			
+				class_sum = np.dot(self.clause_active * self.weight_banks[i].get_weights(), clause_outputs).astype(np.int32)
+				class_sum = np.clip(class_sum, -self.T, self.T)
+
+				type_iii_feedback_selection = np.random.choice(2)
+
+				if Ym[i, e] == 1:
+					update_p = (self.T - class_sum)/(2*self.T)
+
+					self.clause_bank.type_i_feedback(update_p, self.s, self.boost_true_positive_feedback, self.clause_active*(self.weight_banks[i].get_weights() >= 0), self.literal_active, self.encoded_X_train[i], e)
+					self.clause_bank.type_ii_feedback(update_p, self.clause_active*(self.weight_banks[i].get_weights() < 0), self.literal_active, self.encoded_X_train[i], e)
+					self.weight_banks[i].increment(clause_outputs, update_p, self.clause_active, True)
+					if self.type_iii_feedback and type_iii_feedback_selection == 0:
+						self.clause_bank.type_iii_feedback(update_p, self.d, self.clause_active*(self.weight_banks[i].get_weights() >= 0), self.literal_active, self.encoded_X_train[i], e, 1)
+						self.clause_bank.type_iii_feedback(update_p, self.d, self.clause_active*(self.weight_banks[i].get_weights() < 0), self.literal_active, self.encoded_X_train[i], e, 0)
+				else:
+					update_p = (self.T + class_sum)/(2*self.T)
+
+					self.clause_bank.type_i_feedback(update_p, self.s, self.boost_true_positive_feedback, self.clause_active * (self.weight_banks[i].get_weights() < 0), self.literal_active, self.encoded_X_train[i], e)
+					self.clause_bank.type_ii_feedback(update_p, self.clause_active*(self.weight_banks[i].get_weights() >= 0), self.literal_active, self.encoded_X_train[i], e)
+					self.weight_banks[i].decrement(clause_outputs, update_p, self.clause_active, True)
+					if self.type_iii_feedback and type_iii_feedback_selection == 1:
+						self.clause_bank.type_iii_feedback(update_p, self.d, self.clause_active*(self.weight_banks[i].get_weights() < 0), self.literal_active, self.encoded_X_train[i], e, 1)
+						self.clause_bank.type_iii_feedback(update_p, self.d, self.clause_active*(self.weight_banks[i].get_weights() >= 0), self.literal_active, self.encoded_X_train[i], e, 0)
+		return
+
+	def predict(self, X):
+		if not np.array_equal(self.X_test, X):
+			self.encoded_X_test = {}
+			for i in range(self.number_of_classes):
+				self.encoded_X_test[i] = self.clause_bank.prepare_X(X[i])
+			self.X_test = X.copy()
+		
+		Y = np.ascontiguousarray(np.zeros((X.shape[0], X.shape[1]), dtype=np.uint32))
+
+		for e in range(X.shape[0]):
+			for i in range(self.number_of_classes):
+				clause_outputs = self.clause_bank.calculate_clause_outputs_predict(self.encoded_X_test[i], e)			
+				class_sum = np.dot(self.weight_banks[i].get_weights(), clause_outputs).astype(np.int32)
+				Y[i, e] = class_sum >= 0
+		return Y
+
+	def clause_precision(self, the_class, positive_polarity, X, Y):
+		clause_outputs = self.transform(X)
+		weights = self.weight_banks[the_class].get_weights()
+		if positive_polarity == 0:
+			positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
+			true_positive_clause_outputs = clause_outputs[Y==the_class].sum(axis=0)
+			false_positive_clause_outputs = clause_outputs[Y!=the_class].sum(axis=0)
+		else:
+			positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
+			true_positive_clause_outputs = clause_outputs[Y!=the_class].sum(axis=0)
+			false_positive_clause_outputs = clause_outputs[Y==the_class].sum(axis=0)
+		
+		return np.where(true_positive_clause_outputs + false_positive_clause_outputs == 0, 0, 1.0*true_positive_clause_outputs/(true_positive_clause_outputs + false_positive_clause_outputs))
+
+	def clause_recall(self, the_class, positive_polarity, X, Y):
+		clause_outputs = self.transform(X)
+		weights = self.weight_banks[the_class].get_weights()
+		
+		if positive_polarity == 0:
+			positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
+			true_positive_clause_outputs = positive_clause_outputs[Y==the_class].sum(axis=0)
+		else:
+			positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
+			true_positive_clause_outputs = positive_clause_outputs[Y!=the_class].sum(axis=0)
+			
+		return true_positive_clause_outputs / Y[Y==the_class].shape[0]
+
+	def get_weight(self, the_class, clause):
+		return self.weight_banks[the_class].get_weights()[clause]
+
+	def set_weight(self, the_class, clause, weight):
+		self.weight_banks[the_class].get_weights()[clause] = weight
+
+
 class TMMultiChannelClassifier(TMBasis):
 	def __init__(self, number_of_clauses, global_T, T, s, platform = 'CPU', patch_dim=None, feature_negation=True, boost_true_positive_feedback=1, number_of_state_bits_ta=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
 		super().__init__(number_of_clauses, T, s, platform = platform, patch_dim=patch_dim, feature_negation=feature_negation, boost_true_positive_feedback=boost_true_positive_feedback, number_of_state_bits_ta=number_of_state_bits_ta, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
