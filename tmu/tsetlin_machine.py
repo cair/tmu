26fc149f3fa7e7d6eb675feff4343c5c33394aa7
Ole-Christoffer Granmo <ole.granmo@uia.no>
2022-07-31 10:15:55 +0200

    Literal budget for autoencoder

--- tmu/tsetlin_machine.py
+++ tmu/tsetlin_machine.py
@@ -499,10 +499,10 @@ class TMCoalescedClassifier(TMBasis):
 		self.weight_banks[the_class].get_weights()[clause] = weight
 
 class TMAutoEncoder(TMBasis):
-	def __init__(self, number_of_clauses, T, s, output_active, accumulation=1, type_i_ii_ratio = 1.0, type_iii_feedback=False, focused_negative_sampling=False, output_balancing=False, d=200.0, platform = 'CPU', patch_dim=None, feature_negation=True, boost_true_positive_feedback=1, number_of_state_bits_ta=8, number_of_state_bits_ind=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
+	def __init__(self, number_of_clauses, T, s, output_active, accumulation=1, type_i_ii_ratio = 1.0, type_iii_feedback=False, focused_negative_sampling=False, output_balancing=False, d=200.0, platform = 'CPU', patch_dim=None, feature_negation=True, boost_true_positive_feedback=1, max_included_literals=None, number_of_state_bits_ta=8, number_of_state_bits_ind=8, weighted_clauses=False, clause_drop_p = 0.0, literal_drop_p = 0.0):
 		self.output_active = output_active
 		self.accumulation = accumulation
-		super().__init__(number_of_clauses, T, s, type_i_ii_ratio = type_i_ii_ratio, type_iii_feedback=type_iii_feedback, focused_negative_sampling=focused_negative_sampling, output_balancing=output_balancing, d=d, platform = platform, patch_dim=patch_dim, feature_negation=feature_negation, boost_true_positive_feedback=boost_true_positive_feedback, number_of_state_bits_ta=number_of_state_bits_ta, number_of_state_bits_ind=number_of_state_bits_ind, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
+		super().__init__(number_of_clauses, T, s, type_i_ii_ratio = type_i_ii_ratio, type_iii_feedback=type_iii_feedback, focused_negative_sampling=focused_negative_sampling, output_balancing=output_balancing, d=d, platform = platform, patch_dim=patch_dim, feature_negation=feature_negation, boost_true_positive_feedback=boost_true_positive_feedback, max_included_literals=max_included_literals, number_of_state_bits_ta=number_of_state_bits_ta, number_of_state_bits_ind=number_of_state_bits_ind, weighted_clauses=weighted_clauses, clause_drop_p = clause_drop_p, literal_drop_p = literal_drop_p)
 
 	def initialize(self, X):
 		self.number_of_classes = self.output_active.shape[0]
@@ -519,6 +519,9 @@ class TMAutoEncoder(TMBasis):
 		for i in range(self.number_of_classes):
 			self.weight_banks.append(WeightBank(np.random.choice([-1,1], size=self.number_of_clauses).astype(np.int32)))
 	
+		if self.max_included_literals == None:
+			self.max_included_literals = self.clause_bank.number_of_literals
+
 	def update(self, target_output, target_value, encoded_X, clause_active, literal_active):
 		all_literal_active = (np.zeros(self.clause_bank.number_of_ta_chunks, dtype=np.uint32) | ~0).astype(np.uint32)
 		clause_outputs = self.clause_bank.calculate_clause_outputs_update(all_literal_active, encoded_X, 0)
@@ -531,7 +534,7 @@ class TMAutoEncoder(TMBasis):
 		if target_value == 1:
 			update_p = (self.T - class_sum)/(2*self.T)
 
-			self.clause_bank.type_i_feedback(update_p*self.type_i_p, self.s, self.boost_true_positive_feedback, clause_active*(self.weight_banks[target_output].get_weights() >= 0), literal_active, encoded_X, 0)
+			self.clause_bank.type_i_feedback(update_p*self.type_i_p, self.s, self.boost_true_positive_feedback, self.max_included_literals, clause_active*(self.weight_banks[target_output].get_weights() >= 0), literal_active, encoded_X, 0)
 			self.clause_bank.type_ii_feedback(update_p*self.type_ii_p, clause_active*(self.weight_banks[target_output].get_weights() < 0), literal_active, encoded_X, 0)
 			self.weight_banks[target_output].increment(clause_outputs, update_p, clause_active, True)
 			if self.type_iii_feedback and type_iii_feedback_selection == 0:
@@ -540,7 +543,7 @@ class TMAutoEncoder(TMBasis):
 		else:
 			update_p = (self.T + class_sum)/(2*self.T)
 	
-			self.clause_bank.type_i_feedback(update_p*self.type_i_p, self.s, self.boost_true_positive_feedback, clause_active * (self.weight_banks[target_output].get_weights() < 0), literal_active, encoded_X, 0)
+			self.clause_bank.type_i_feedback(update_p*self.type_i_p, self.s, self.boost_true_positive_feedback, self.max_included_literals, clause_active * (self.weight_banks[target_output].get_weights() < 0), literal_active, encoded_X, 0)
 			self.clause_bank.type_ii_feedback(update_p*self.type_ii_p, clause_active*(self.weight_banks[target_output].get_weights() >= 0), literal_active, encoded_X, 0)
 			self.weight_banks[target_output].decrement(clause_outputs, update_p, clause_active, True)
 			if self.type_iii_feedback and type_iii_feedback_selection == 1:
@@ -634,32 +637,38 @@ class TMAutoEncoder(TMBasis):
 
 		return literal_frequency
 
-	def clause_precision(self, the_class, positive_polarity, X, Y):
+	def clause_precision(self, positive_polarity, X, Y):
 		clause_outputs = self.transform(X)
-		weights = self.weight_banks[the_class].get_weights()
-		if positive_polarity == 0:
-			positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
-			true_positive_clause_outputs = clause_outputs[Y==the_class].sum(axis=0)
-			false_positive_clause_outputs = clause_outputs[Y!=the_class].sum(axis=0)
-		else:
-			positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
-			true_positive_clause_outputs = clause_outputs[Y!=the_class].sum(axis=0)
-			false_positive_clause_outputs = clause_outputs[Y==the_class].sum(axis=0)
-		
-		return np.where(true_positive_clause_outputs + false_positive_clause_outputs == 0, 0, 1.0*true_positive_clause_outputs/(true_positive_clause_outputs + false_positive_clause_outputs))
 
-	def clause_recall(self, the_class, positive_polarity, X, Y):
+		precision = np.empty(self.number_of_classes, number_of_clauses)
+		for i in range(self.number_of_classes):
+			weights = self.weight_banks[i].get_weights()
+			if positive_polarity == 0:
+				positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
+				true_positive_clause_outputs = positive_clause_outputs[Y==i].sum(axis=0)
+				false_positive_clause_outputs = positive_clause_outputs[Y!=i].sum(axis=0)
+			else:
+				positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
+				true_positive_clause_outputs = clause_outputs[Y!=i].sum(axis=0)
+				false_positive_clause_outputs = clause_outputs[Y==i].sum(axis=0)
+			precision[i,:] = np.where(true_positive_clause_outputs + false_positive_clause_outputs == 0, 0, 1.0*true_positive_clause_outputs/(true_positive_clause_outputs + false_positive_clause_outputs))
+		return precision
+
+	def clause_recall(self, positive_polarity, X, Y):
 		clause_outputs = self.transform(X)
-		weights = self.weight_banks[the_class].get_weights()
+
+		recall = np.empty(self.number_of_classes, number_of_clauses)
+		for i in range(self.number_of_classes):
+			weights = self.weight_banks[i].get_weights()
 		
-		if positive_polarity == 0:
-			positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
-			true_positive_clause_outputs = positive_clause_outputs[Y==the_class].sum(axis=0)
-		else:
-			positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
-			true_positive_clause_outputs = positive_clause_outputs[Y!=the_class].sum(axis=0)
+			if positive_polarity == 0:
+				positive_clause_outputs = (weights >= 0)[:,np.newaxis].transpose() * clause_outputs
+				recall[i,:] = positive_clause_outputs[Y==the_class].sum(axis=0) / Y[Y==the_class].shape[0]
+			else:
+				positive_clause_outputs = (weights < 0)[:,np.newaxis].transpose() * clause_outputs
+				recall[i,:] = positive_clause_outputs[Y!=the_class].sum(axis=0) / Y[Y!=the_class].shape[0]
 			
-		return true_positive_clause_outputs / Y[Y==the_class].shape[0]
+		return recall
 
 	def get_weight(self, the_class, clause):
 		return self.weight_banks[the_class].get_weights()[clause]
