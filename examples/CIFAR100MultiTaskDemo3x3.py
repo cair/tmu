ce2b3bbb80d6bd333e572eeb3ea5c4b6dbfc6390
Ole-Christoffer Granmo <ole.granmo@uia.no>
2022-08-14 15:28:51 +0200

    Multitask CIFAR100 Demo

--- examples/CIFAR100MultiTaskDemo3x3.py
+++ examples/CIFAR100MultiTaskDemo3x3.py
@@ -41,10 +41,12 @@ X_test_multi_task = {}
 Y_train_multi_task = {}
 Y_test_multi_task = {}
 for i in range(100):
-	other_index = np.arange((Y_train!=i).shape[0])
+	other_index_train = np.arange((Y_train!=i).sum())
 	np.random.shuffle(other_index)
-	X_train_multi_task[i] = np.concatenate(X_train[Y_train==i], X_train[Y_train!=i][other_index[:(Y_train==i).sum()]])
-	X_test_multi_task[i] = np.concatenate(X_test[Y_test==i], X_test[Y_test!=i][other_index[:(Y_test==i).sum()]])
+	other_index_test = np.arange((Y_test!=i).sum())
+	np.random.shuffle(other_index_test)
+	X_train_multi_task[i] = np.concatenate(X_train[Y_train==i], X_train[Y_train!=i][other_index_train[:(Y_train==i).sum()]])
+	X_test_multi_task[i] = np.concatenate(X_test[Y_test==i], X_test[Y_test!=i][other_index_test[:(Y_test==i).sum()]])
 	Y_train_multi_task[i] = np.concatenate(np.ones((Y_train==i).sum(), dtype=np.uint32), np.zeros((Y_train==i).sum(), dtype=np.uint32))
 	Y_test_multi_task[i] = np.concatenate(np.ones((Y_test==i).sum(), dtype=np.uint32), np.zeros((Y_test==i).sum(), dtype=np.uint32))
 
