0c98b32485bc5122cc4d78654a4e9b514ff40645
Ole-Christoffer Granmo <ole.granmo@uia.no>
2022-12-02 12:42:53 +0100

    Updated MNIST Literal Budget Demo

--- examples/MNISTLiteralBudgetDemo.py
+++ examples/MNISTLiteralBudgetDemo.py
@@ -5,27 +5,42 @@ from keras.datasets import mnist
 
 from tmu.tsetlin_machine import TMClassifier
 
+ensembles = 5
+epochs = 250
+
+clauses = 8000
+T = 10000
+s = 5.0
+max_included_literals = 5
+
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0) 
 X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0) 
 
-tm = TMClassifier(2000, 50*100, 10.0, platform='CPU', max_included_literals=5, weighted_clauses=True)
+tm = TMClassifier(clauses, T, s, platform='CUDA', max_included_literals=max_included_literals, weighted_clauses=True)
 
-print("\nAccuracy over 250 epochs:\n")
-for e in range(250):
-        start_training = time()
-        tm.fit(X_train, Y_train)
-        stop_training = time()
 
-        start_testing = time()
-        result_test = 100*(tm.predict(X_test) == Y_test).mean()
-        stop_testing = time()
+f = open("mnist_%.1f_%d_%d_%d.txt" % (s, clauses, T, max_included_literals), "w+")
+for ensemble in range(ensembles):
+        print("\nAccuracy over %d epochs:\n" % (epochs))
 
-        number_of_includes = 0
-        for i in range(10):
-                for j in range(2000):
-                        number_of_includes += tm.number_of_include_actions(i, j)
-        number_of_includes /= 10*2000
+        for epoch in range(epochs):
+                start_training = time()
+                tm.fit(X_train, Y_train)
+                stop_training = time()
 
-        print("#%d Accuracy: %.2f%% Includes: %.1f Training: %.2fs Testing: %.2fs" % (e+1, result_test, number_of_includes, stop_training-start_training, stop_testing-start_testing))
+                start_testing = time()
+                result_test = 100*(tm.predict(X_test) == Y_test).mean()
+                stop_testing = time()
+
+                number_of_includes = 0
+                for i in range(10):
+                        for j in range(clauses):
+                                number_of_includes += tm.number_of_include_actions(i, j)
+                number_of_includes /= 10*clauses
+
+                print("%d %d %.2f %.2f %.2f %.2f %.2f" % (ensembe, epoch, number_of_includes, result_test, stop_training-start_training, stop_testing-start_testing))
+                print("%d %d %.2f %.2f %.2f %.2f %.2f" % (ensembe, epoch, number_of_includes, result_test, stop_training-start_training, stop_testing-start_testing), file=f)
+                f.flush()
+f.close()
