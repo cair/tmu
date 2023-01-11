b71500c6670bec83c2b50613a58377b06bed53e2
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-01-11 11:46:44 +0100

    Windows support

--- tmu/clause_bank_extension_build.py
+++ tmu/clause_bank_extension_build.py
@@ -22,28 +22,21 @@
 # https://arxiv.org/abs/1905.09688
 
 from cffi import FFI
-ffibuilder = FFI()
+import pathlib
+current_dir = pathlib.Path(__file__).parent
 
-ffibuilder.cdef("""
-    void cb_calculate_clause_outputs_predict(unsigned int *ta_state, int number_of_clauses, int number_of_features, int number_of_state_bits, int number_of_patches, unsigned int *clause_output, unsigned int *Xi);
-    void cb_calculate_clause_outputs_update(unsigned int *ta_state, int number_of_clauses, int number_of_features, int number_of_state_bits, int number_of_patches, unsigned int *clause_output, unsigned int *literal_active, unsigned int *Xi);
-    void cb_calculate_clause_outputs_patchwise(unsigned int *ta_state, int number_of_clauses, int number_of_features, int number_of_state_bits, int number_of_patches, unsigned int *clause_output, unsigned int *Xi);
-    void cb_calculate_literal_frequency(unsigned int *ta_state, int number_of_clauses, int number_of_features, int number_of_state_bits, unsigned int *clause_active, unsigned int *literal_count);
-    int cb_number_of_include_actions(unsigned int *ta_state, int clause, int number_of_features, int number_of_state_bits);
-    void cb_type_i_feedback(unsigned int *ta_state, unsigned int *feedback_to_ta, unsigned int *output_one_patches, int number_of_clauses, int number_of_features, int number_of_state_bits, int number_of_patches, float update_p, float s, unsigned int boost_true_positive_feedback, unsigned int max_included_literals, unsigned int *clause_active, unsigned int *literal_active, unsigned int *Xi);
-    void cb_type_ii_feedback(unsigned int *ta_state, unsigned int *output_one_patches, int number_of_clauses, int number_of_features, int number_of_state_bits, int number_of_patches, float update_p, unsigned int *clause_active, unsigned int *literal_active, unsigned int *Xi);
-    void cb_type_iii_feedback(unsigned int *ta_state, unsigned int *ind_state, unsigned int *clause_and_target, unsigned int *output_one_patches, int number_of_clauses, int number_of_features, int number_of_state_bits_ta, int number_of_state_bits_ind, int number_of_patches, float update_p, float d, unsigned int *clause_active, unsigned int *literal_active, unsigned int *Xi, unsigned int target);
-    void cb_initialize_incremental_clause_calculation(unsigned int *ta_state, unsigned int *literal_clause_map, unsigned int *literal_clause_map_pos, unsigned int *false_literals_per_clause, int number_of_clauses, int number_of_literals, int number_of_state_bits, unsigned int *previous_Xi);
-    void cb_calculate_clause_outputs_incremental_batch(unsigned int * literal_clause_map, unsigned int *literal_clause_map_pos, unsigned int *false_literals_per_clause, int number_of_clauses, int number_of_literals, int number_of_patches, unsigned int *clause_output, unsigned int *previous_Xi, unsigned int *Xi, int batch_size);
-    void cb_calculate_clause_outputs_incremental(unsigned int * literal_clause_map, unsigned int *literal_clause_map_pos, unsigned int *false_literals_per_clause, int number_of_clauses, int number_of_literals, unsigned int *previous_Xi, unsigned int *Xi);
-    """)
+ffibuilder  = FFI()
 
-ffibuilder.set_source("tmu._cb",  # name of the output C extension
-"""
-    #include "./tmu/ClauseBank.h"
-""",
-    include_dirs=['.'],
-    sources=['./tmu/ClauseBank.c']) 
+with current_dir.joinpath("ClauseBank.h").open("r") as f:
+    ffibuilder .cdef(f.read())
+
+with current_dir.joinpath("ClauseBank.c").open("r") as f:
+    ffibuilder .set_source(
+        "tmu._cb",
+        f.read(),
+        include_dirs=[current_dir.absolute()],
+        source_extension=".c",
+)
 
 if __name__ == "__main__":
-    ffibuilder.compile(verbose=True)
+    ffibuilder .compile(verbose=True)
