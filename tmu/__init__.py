db4cdaf054af6c454221f4160d891f97cb009fbf
Per-Arne Andersen <per@sysx.no>
2023-01-12 15:53:16 +0100

    Added check for properly set up environment. Errors out with instructions if CFFI libraries are missing.

--- tmu/__init__.py
+++ tmu/__init__.py
@@ -1,1 +1,8 @@
 
+
+try:
+    import tmu._cb
+    import tmu._tools
+    import tmu._wb
+except ImportError:
+    raise ImportError("Could not import cffi compiled libraries. To fix this problem, run pip install -e .")
