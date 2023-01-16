de914286fccddd850f4f10c5c9a97cc8e8211971
Per-Arne Andersen <per@sysx.no>
2023-01-16 17:19:04 +0100

    Fixed import test.

--- test/test_imports.py
+++ test/test_imports.py
@@ -1,11 +1,5 @@
 
 
 
-def test_import_clause_bank():
-    import tmu._cb
-
-def test_import_tools():
-    import tmu._tools
-
-def test_import_weight_bank():
-    import tmu._wb
+def test_import_tmulib():
+    import tmu.tmulib
