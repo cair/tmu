762c234ac562a47737b5de7af77f8dbc88b3e6fa
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-01-20 23:18:54 +0100

    Update

--- examples/classification/MNISTConvolutionDemo.py
+++ examples/classification/MNISTConvolutionDemo.py
@@ -7,8 +7,8 @@ from tmu.models.classification.vanilla_classifier import TMClassifier
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
-X_train = np.where(X_train.reshape((X_train.shape[0], 28*28)) > 75, 1, 0)
-X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)
+X_train = np.where(X_train  > 75, 1, 0)
+X_test = np.where(X_test > 75, 1, 0)
 
 tm = TMClassifier(8000, 10000, 5.0, patch_dim=(10, 10), max_included_literals=32, platform='CUDA', weighted_clauses=True)
 
