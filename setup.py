67f9b7898da3e3f176290c9b0b6f39a0ebb4f098
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-02-01 00:01:22 +0100

    Added combined clause and weight bank

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ current_dir = Path(__file__).parent
 
 setup(
     name='tmu',
-    version='0.7.3',
+    version='0.7.4',
     url='https://github.com/cair/tmu/',
     author='Ole-Christoffer Granmo',
     author_email='ole.granmo@uia.no',
