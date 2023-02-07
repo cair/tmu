ffe7ef004e78ad54f420df2fe35733172f1ed12e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-02-07 11:44:18 +0100

    Under development

--- examples/experimental/relational/RelationalTMDemo.py
+++ examples/experimental/relational/RelationalTMDemo.py
@@ -1,1 +1,20 @@
+import numpy as np
+import keras
+from time import time
 
+from tmu.models.relational.vanilla_relational import TMRelational
+
+X = [
+		(("Parent", "Mary", "Bob"), ("Parent", "Bob", "Peter"), ("Ancestor", "Mary", "Bob"), ("Ancestor", "Bob", "Peter"), ("Ancestor", "Mary", "Peter")),
+		(("Parent", "Ida", "Chris"), ("Parent", "Chris", "Ann"), ("Ancestor", "Ida", "Chris"), ("Ancestor", "Chris", "Ann"), ("Ancestor", "Ida", "Ann"))
+	]
+
+clauses = 20
+T = 40
+s = 5.0
+
+print("Number of clauses:", clauses)
+
+tm = TMRelational(clauses, T, s, output_active, max_included_literals=3, feature_negation=False, platform='CPU', output_balancing=True)
+
+tm.propositionalize_relations(X)
