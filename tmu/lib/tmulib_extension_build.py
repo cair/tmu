3fa2ca3b982cd6a2e03c7fb2dd9ba07fd5411f54
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-03-07 11:40:43 +0100

    Sparse clause bank implemented in C

--- tmu/lib/tmulib_extension_build.py
+++ tmu/lib/tmulib_extension_build.py
@@ -28,8 +28,8 @@ current_dir = pathlib.Path(__file__).parent
 source_dir = current_dir.joinpath("src")
 header_dir = current_dir.joinpath("include")
 
-HEADERS = ["ClauseBank.h", "Tools.h", "WeightBank.h", "ClauseWeightBank.h"]
-SOURCES = ["ClauseBank.c", "Tools.c", "WeightBank.c", "ClauseWeightBank.c"]
+HEADERS = ["ClauseBank.h", "Tools.h", "WeightBank.h", "ClauseWeightBank.h", "ClauseBankSparse.h"]
+SOURCES = ["ClauseBank.c", "Tools.c", "WeightBank.c", "ClauseWeightBank.c", "ClauseBankSparse.c"]
 
 header_content = '\n'.join([header_dir.joinpath(x).open("r").read() for x in HEADERS])
 source_content = '\n'.join([source_dir.joinpath(x).open("r").read() for x in SOURCES])
