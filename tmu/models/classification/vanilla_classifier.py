d84b81d9b5bd3d22fbd92f87e071469d9432b050
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-03-07 12:33:12 +0100

    Moved import of ClauseBankCUDA to creation of object to avoid warning

--- tmu/models/classification/vanilla_classifier.py
+++ tmu/models/classification/vanilla_classifier.py
@@ -16,7 +16,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from tmu.clause_bank.clause_bank_cuda import ClauseBankCUDA
+
 from tmu.clause_bank.clause_bank import ClauseBank
 from tmu.clause_bank.clause_bank_sparse import ClauseBankSparse
 
@@ -89,6 +89,7 @@ class TMClassifier(TMBaseClassifier):
                 incremental=self.incremental
             )
         elif self.platform in ["GPU", "CUDA"]:
+            from tmu.clause_bank.clause_bank_cuda import ClauseBankCUDA
             clause_bank_type = ClauseBankCUDA
             clause_bank_args = dict(
                 X=X,
