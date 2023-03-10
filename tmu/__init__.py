cd14aca096510719d041f821a95516e5bb737b57
Per-Arne Andersen <per@sysx.no>
2023-03-10 14:20:54 +0100

    Added temporary fix for logging error.

--- tmu/__init__.py
+++ tmu/__init__.py
@@ -1,13 +1,23 @@
 import logging.config
+import logging
 import pathlib
 import json
 
 
+_LOGGER = logging.getLogger(__name__)
+
+
 if not logging.getLogger().handlers:
-    # TODO add argparse configs here.
+
     _current_file = pathlib.Path(__file__).parent
-    with _current_file.joinpath("logging_example.json").open("r") as config_file:
-        logging.config.dictConfig(json.load(config_file))
+    _default_logging_file = _current_file.joinpath("logging_example.json")
+    if _default_logging_file.exists():
+        with _default_logging_file.open("r") as config_file:
+            logging.config.dictConfig(json.load(config_file))
+    else:
+        _LOGGER.warning(f"Could not find default_logging file: {_default_logging_file.absolute()}. This is most "
+                        f"likely a bug.")
+
 
 try:
     import tmu.tmulib
