a4d48ca3f9691f441dd884c7d7894ee05d63ddb0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-03-12 15:05:39 +0100

    Absorbing include

--- examples/classification/MNISTSparseAbsorbingDemo.py
+++ examples/classification/MNISTSparseAbsorbingDemo.py
@@ -13,7 +13,7 @@ X_test = np.where(X_test.reshape((X_test.shape[0], 28*28)) > 75, 1, 0)#[0:1000]
 Y_train = Y_train#[0:1000]
 Y_test = Y_test#[0:1000]
 
-tm = TMClassifier(2000, 5000, 10.0, max_included_literals=32, platform='CPU_sparse', weighted_clauses=True, absorbing=75)
+tm = TMClassifier(2000, 5000, 10.0, max_included_literals=32, platform='CPU_sparse', weighted_clauses=True, absorbing_include=150, absorbing_exclude=90)
 
 print("\nAccuracy over 60 epochs:\n")
 for e in range(60):
@@ -25,10 +25,19 @@ for e in range(60):
         result = 100*(tm.predict(X_test) == Y_test).mean()
         stop_testing = time()
 
-        absorbed = 0.0
+        absorbed_exclude = 0.0
         for i in range(10):
                 for j in range(2000):
-                        absorbed += 1.0 - (tm.number_of_include_actions(i, j) + tm.number_of_exclude_actions(i, j)) / (X_train.shape[1]*2)
-        absorbed = 100 * absorbed / (10*2000)
+                        absorbed_exclude += 1.0 - (tm.number_of_exclude_actions(i, j)) / (X_train.shape[1]*2 - tm.number_of_absorbed_include_actions(i, j) - tm.number_of_include_actions(i, j))
+        absorbed_exclude = 100 * absorbed_exclude / (10*2000)
 
-        print("#%d Accuracy: %.2f%% Absorbed: %.2f%% Training: %.2fs Testing: %.2fs" % (e+1, result, absorbed, stop_training-start_training, stop_testing-start_testing))
+        absorbed_include = 0.0
+        include_counter = 0
+        for i in range(10):
+                for j in range(2000):
+                        if tm.number_of_absorbed_include_actions(i, j) + tm.number_of_include_actions(i, j) > 0:
+                                absorbed_include += 1.0 * tm.number_of_absorbed_include_actions(i, j) / (tm.number_of_absorbed_include_actions(i, j) + tm.number_of_include_actions(i, j))
+                                include_counter += 1
+        absorbed_include = 100 * absorbed_include / include_counter
+
+        print("#%d Accuracy: %.2f%% Absorbed Exclude: %.2f%% Absorbed Include: %.2f%% Training: %.2fs Testing: %.2fs" % (e+1, result, absorbed_exclude, absorbed_include, stop_training-start_training, stop_testing-start_testing))
