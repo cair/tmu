7470e9326ab0d36da67e41e619f51a3ce1285728
Per-Arne Andersen <per.andersen@uia.no>
2023-06-21 11:19:00 +0000

    * Removed Type III experiments from tmu and relocated to separate git * Added module file for examples.

--- examples/type_iii_feedback/toy_1/dashboard.sh
+++
@@ -1,5 +0,0 @@
-if [ -f .env ]; then
-  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
-fi
-#conda activate tmu
-optuna-dashboard mysql+pymysql://$MYSQL_USER:$MYSQL_PASS@localhost/$MYSQL_DB --host=0.0.0.0
