acd3638c93ca954af811f564054301c093926f7e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2023-07-03 23:00:38 +0200

    Experimenting w/color histograms

--- examples/experimental/classification/CIFAR2Histogram.py
+++ examples/experimental/classification/CIFAR2Histogram.py
@@ -13,12 +13,6 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 from keras.datasets import cifar10
 
-scaling = 1.0
-unique_patches = 2**9
-hypervector_size = int(512*scaling)
-bits = np.maximum(5, int(5*scaling))
-print(bits)
-
 resolution = 8
 
 indexes = np.arange(hypervector_size, dtype=np.uint32)
@@ -33,11 +27,14 @@ ensembles = 5
 epochs = 250
 
 max_included_literals = 32
-clauses = 2000
-T = 5000
-s = 1.5
+clauses = 8000
+T = int(clauses * 0.75)
+s = 10.0
+patch_size = 3
+resolution = 8
+number_of_state_bits_ta = 8
+literal_drop_p = 0.0
 step = 1
-visual_tokens = True
 
 (X_train_org, Y_train), (X_test_org, Y_test) = cifar10.load_data()
 X_train_org = X_train_org.astype(np.uint32)
@@ -48,49 +45,35 @@ Y_test = Y_test.reshape(Y_test.shape[0])
 Y_train = np.where(np.isin(Y_train, animals), 1, 0)
 Y_test = np.where(np.isin(Y_test, animals), 1, 0)
 
-X_train = np.empty((X_train_org.shape[0], X_train_org.shape[1], X_train_org.shape[2], X_train_org.shape[3], resolution),
-                   dtype=np.uint8)
-for z in range(resolution):
-    X_train[:, :, :, :, z] = X_train_org[:, :, :, :] >= (z + 1) * 255 / (resolution + 1)
+X_train = np.zeros((X_train_org.shape[0], 30//step, 30//step, resolution**3), dtype=np.uint32)
+for i in range(X_train.shape[0]):
+        windows = view_as_windows(X_train[i,:,:,c,z], (patch_size, patch_size), step=step)
+        for u in range(windows.shape[0]):
+                for v in range(windows.shape[1]):
+                        patch = windows[u,v].reshape(-1).astype(np.uint32)
+                        for x in range(patch_size):
+                                for y in range(patch_size):
+                                        color_id = (patch[x, y, 0]//(256/8)) * (resolution**2) +
+                                                (patch[x, y, 1]//(256/8)) * resolution +
+                                                (patch[x, y, 2]//(256/8))
+                                        X_train[i, u, v, color_id] = 1
 
-X_test = np.empty((X_test_org.shape[0], X_test_org.shape[1], X_test_org.shape[2], X_test_org.shape[3], resolution),
-                  dtype=np.uint8)
-for z in range(resolution):
-    X_test[:, :, :, :, z] = X_test_org[:, :, :, :] >= (z + 1) * 255 / (resolution + 1)
+print("Training data produced")
 
-if visual_tokens:
-        X_train_tokenized = np.zeros((X_train.shape[0], 30//step, 30//step, hypervector_size), dtype=np.uint32)
+X_test = np.zeros((X_test_org.shape[0], 30//step, 30//step, resolution**3), dtype=np.uint32)
+for i in range(X_test.shape[0]):
+        windows = view_as_windows(X_test[i,:,:,c,z], (patch_size, patch_size), step=step)
+        for u in range(windows.shape[0]):
+                for v in range(windows.shape[1]):
+                        patch = windows[u,v].reshape(-1).astype(np.uint32)
+                        for x in range(patch_size):
+                                for y in range(patch_size):
+                                        color_id = (patch[x, y, 0]//(256/8)) * (resolution**2) +
+                                                (patch[x, y, 1]//(256/8)) * resolution +
+                                                (patch[x, y, 2]//(256/8))
+                                        X_test[i, u, v, color_id] = 1
 
-        for i in range(X_train.shape[0]):
-                for c in range(3):
-                        for z in range(resolution):
-                                windows = view_as_windows(X_train[i,:,:,c,z], (3, 3), step=step)
-                                for u in range(windows.shape[0]):
-                                        for v in range(windows.shape[1]):
-                                                patch = windows[u,v].reshape(-1).astype(np.uint32)
-                                                patch_id = patch.dot(1 << np.arange(patch.shape[-1] - 1, -1, -1))
-                                                X_train_tokenized[i, u, v, :] |= np.roll(encoding[patch_id], c + z*10)
-        print("Training data produced")
-
-        X_test_tokenized = np.zeros((X_test.shape[0], 30//step, 30//step, hypervector_size), dtype=np.uint32)
-
-        for i in range(X_test.shape[0]):
-                for c in range(3):
-                        for z in range(resolution):
-                                windows = view_as_windows(X_test[i,:,:,c,z], (3, 3), step=step)
-                                for u in range(windows.shape[0]):
-                                        for v in range(windows.shape[1]):
-                                                patch = windows[u,v].reshape(-1).astype(np.uint32)
-                                                patch_id = patch.dot(1 << np.arange(patch.shape[-1] - 1, -1, -1))
-                                                X_test_tokenized[i, u, v, :] |= np.roll(encoding[patch_id], c + z*10)
-
-        print("Testing data produced")
-
-        X_train = X_train_tokenized
-        X_test = X_test_tokenized
-else:
-        X_train = X_train.reshape((X_train.shape[0], X_train.shape[1], X_train.shape[2], -1))
-        X_test = X_test.reshape((X_test.shape[0], X_test.shape[1], X_test.shape[2], -1))
+print("Testing data produced")
 
 print(X_train.shape, X_test.shape)
 
@@ -98,11 +81,8 @@ f = open("cifar2_%.1f_%d_%d_%d_%d_%d.txt" % (s, clauses, T, step, visual_tokens,
 for ensemble in range(ensembles):
         print("\nAccuracy over %d epochs:\n" % (epochs))
 
-        if visual_tokens:
-                tm = TMClassifier(clauses, T, s, max_included_literals=max_included_literals, patch_dim=(1,1), platform='CPU', weighted_clauses=True)
-        else:
-                tm = TMClassifier(clauses, T, s, max_included_literals=max_included_literals, patch_dim=(3,3), platform='CPU', weighted_clauses=True)
-
+        tm = TMClassifier(clauses, T, s, max_included_literals=max_included_literals, patch_dim=(1,1), platform='GPU', weighted_clauses=True)
+      
         for epoch in range(epochs):
                 start_training = time()
                 tm.fit(X_train, Y_train)
