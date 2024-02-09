ed4d1b350ab1da0cec9cb1316363a5e36f411e6e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-02-09 09:46:57 +0100

    Update

--- examples/autoencoder/DimensionalityReductionDemo.py
+++ examples/autoencoder/DimensionalityReductionDemo.py
@@ -27,7 +27,7 @@ output_active = np.array([0, 1], dtype=np.uint32)
 X_train = np.random.randint(0, 2, size=(5000, number_of_features), dtype=np.uint32)
 X_train[:,0] = np.where(np.random.rand(5000) <= noise, 1- X_train[:,1],  X_train[:,1])
 
-tm = TMAutoEncoder(clauses, T, s, output_active, max_included_literals=3, accumulation=accumulation, feature_negation=True, platform='CPU', output_balancing=True)
+tm = TMAutoEncoder(clauses, T, s, output_active, max_included_literals=3, accumulation=accumulation, feature_negation=True, platform='CPU', output_balancing=0.5)
 
 print("\nAccuracy Over 40 Epochs:")
 for e in range(40):
