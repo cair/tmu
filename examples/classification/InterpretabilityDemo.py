e2de2a09f6a1924e68121c37ccb8221ef8692da0
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-02-09 09:49:50 +0100

    Update

--- examples/classification/InterpretabilityDemo.py
+++ examples/classification/InterpretabilityDemo.py
@@ -2,6 +2,10 @@ from tmu.models.classification.vanilla_classifier import TMClassifier
 import numpy as np
 from time import time
 
+number_of_clauses = 10
+T = 10
+s = 3.0
+
 number_of_features = 20
 noise = 0.1
 
@@ -12,7 +16,7 @@ Y_train = np.where(np.random.rand(5000) <= noise, 1-Y_train, Y_train) # Adds noi
 X_test = np.random.randint(0, 2, size=(5000, number_of_features), dtype=np.uint32)
 Y_test = np.logical_xor(X_test[:,0], X_test[:,1]).astype(dtype=np.uint32)
 
-tm = TMClassifier(10, 15, 3.0, platform='CPU', boost_true_positive_feedback=0)
+tm = TMClassifier(number_of_clauses, T, s, platform='CPU', boost_true_positive_feedback=0)
 
 for i in range(20):
 	tm.fit(X_train, Y_train)
@@ -26,15 +30,20 @@ print("\nClass 0 Positive Clauses:\n")
 precision = tm.clause_precision(0, 0, X_test, Y_test)
 recall = tm.clause_recall(0, 0, X_test, Y_test)
 
-for j in range(5):
+print("\nClass 0 Positive Clauses:\n")
+
+precision = tm.clause_precision(0, 0, X_test, Y_test)
+recall = tm.clause_recall(0, 0, X_test, Y_test)
+
+for j in range(number_of_clauses//2):
 	print("Clause #%d W:%d P:%.2f R:%.2f " % (j, tm.get_weight(0, 0, j), precision[j], recall[j]), end=' ')
 	l = []
 	for k in range(number_of_features*2):
 		if tm.get_ta_action(j, k, the_class = 0, polarity = 0):
 			if k < number_of_features:
-				l.append(" x%d" % (k))
+				l.append(" x%d(%d)" % (k, tm.get_ta_state(j, k, the_class = 1, polarity = 0)))
 			else:
-				l.append("¬x%d" % (k-number_of_features))
+				l.append("¬x%d(%d)" % (k-number_of_features, tm.get_ta_state(j, k, the_class = 0, polarity = 0)))
 	print(" ∧ ".join(l))
 
 print("\nClass 0 Negative Clauses:\n")
@@ -42,15 +51,15 @@ print("\nClass 0 Negative Clauses:\n")
 precision = tm.clause_precision(0, 1, X_test, Y_test)
 recall = tm.clause_recall(0, 1, X_test, Y_test)
 
-for j in range(5):
+for j in range(number_of_clauses//2):
 	print("Clause #%d W:%d P:%.2f R:%.2f " % (j, tm.get_weight(0, 1, j), precision[j], recall[j]), end=' ')
 	l = []
 	for k in range(number_of_features*2):
 		if tm.get_ta_action(j, k, the_class = 0, polarity = 1):
 			if k < number_of_features:
-				l.append(" x%d" % (k))
+				l.append(" x%d(%d)" % (k, tm.get_ta_state(j, k, the_class = 0, polarity = 1)))
 			else:
-				l.append("¬x%d" % (k-number_of_features))
+				l.append("¬x%d(%d)" % (k-number_of_features, tm.get_ta_state(j, k, the_class = 0, polarity = 1)))
 	print(" ∧ ".join(l))
 
 print("\nClass 1 Positive Clauses:\n")
@@ -58,15 +67,15 @@ print("\nClass 1 Positive Clauses:\n")
 precision = tm.clause_precision(1, 0, X_test, Y_test)
 recall = tm.clause_recall(1, 0, X_test, Y_test)
 
-for j in range(5):
+for j in range(number_of_clauses//2):
 	print("Clause #%d W:%d P:%.2f R:%.2f " % (j, tm.get_weight(1, 0, j), precision[j], recall[j]), end=' ')
 	l = []
 	for k in range(number_of_features*2):
 		if tm.get_ta_action(j, k, the_class = 1, polarity = 0):
 			if k < number_of_features:
-				l.append(" x%d" % (k))
+				l.append(" x%d(%d)" % (k, tm.get_ta_state(j, k, the_class = 1, polarity = 0)))
 			else:
-				l.append("¬x%d" % (k-number_of_features))
+				l.append("¬x%d(%d)" % (k-number_of_features, tm.get_ta_state(j, k, the_class = 1, polarity = 0)))
 	print(" ∧ ".join(l))
 
 print("\nClass 1 Negative Clauses:\n")
@@ -74,15 +83,15 @@ print("\nClass 1 Negative Clauses:\n")
 precision = tm.clause_precision(1, 1, X_test, Y_test)
 recall = tm.clause_recall(1, 1, X_test, Y_test)
 
-for j in range(5):
+for j in range(number_of_clauses//2):
 	print("Clause #%d W:%d P:%.2f R:%.2f " % (j, tm.get_weight(1, 1, j), precision[j], recall[j]), end=' ')
 	l = []
 	for k in range(number_of_features*2):
 		if tm.get_ta_action(j, k, the_class = 1, polarity = 1):
 			if k < number_of_features:
-				l.append(" x%d" % (k))
+				l.append(" x%d(%d)" % (k, tm.get_ta_state(j, k, the_class = 1, polarity = 1)))
 			else:
-				l.append("¬x%d" % (k-number_of_features))
+				l.append("¬x%d(%d)" % (k-number_of_features, tm.get_ta_state(j, k, the_class = 1, polarity = 1)))
 	print(" ∧ ".join(l))
 
 print("\nClause Co-Occurence Matrix:\n")
