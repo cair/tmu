d3a2838babf16bdaf68ac327583627b686a10277
Per-Arne Andersen <per@sysx.no>
2024-03-04 19:06:27 +0100

    Updated Tool.c; active_output, indrpt_row and indices_row is now const.

--- tmu/lib/src/Tools.c
+++ tmu/lib/src/Tools.c
@@ -35,10 +35,10 @@ unsigned int compareints(const void * a, const void * b)
 }
 
 void tmu_produce_autoencoder_example(
-        unsigned int *active_output,
+        const unsigned int *active_output,
         int number_of_active_outputs,
-        unsigned int *indptr_row,
-        unsigned int *indices_row,
+        const unsigned int *indptr_row,
+        const unsigned int *indices_row,
         int number_of_rows,
         unsigned int *indptr_col,
         unsigned int *indices_col,
