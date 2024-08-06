5445d5dc6d9877bb88cf545a329a93c586ad0656
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-08-06 09:43:03 +0200

    Update

--- examples/classification/SequenceCountInterpretabilityDemo.py
+++ examples/classification/SequenceCountInterpretabilityDemo.py
@@ -34,9 +34,6 @@ def main(args):
         #position_1 = np.random.randint(0, args.sequence_length-4)
         #position_2 = position_1+2
         #position_3 = position_1+4
-
-        #position_2 = np.random.randint(position_1+1, args.sequence_length-1)
-        #position_3 = np.random.randint(position_2+1, args.sequence_length)
         
         if Y_train[i] == 0:
             X_train[i,0,position_1,0] = 1
