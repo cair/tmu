c2105c45ba3452ee1e5b005b5778970c6bf10066
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-08 19:59:59 +0200

    Update

--- tmu/lib/src/ClauseBank.c
+++ tmu/lib/src/ClauseBank.c
@@ -1577,7 +1577,7 @@ void cb_calculate_spatio_temporal_features(
 					unsigned int patch_chunk = patch / 32;
 					unsigned int patch_pos = patch % 32;
 
-					if ((clause_node_output[clause*number_of_patch_chunks + patch_chunk] & (1 < patch_pos)) != clause_value_in_patch[patch] ) {
+					if ((clause_node_output[j*number_of_patch_chunks + patch_chunk] & (1 < patch_pos)) != clause_value_in_patch[patch] ) {
 						printf("ERROR\n");
 					}
 				}
