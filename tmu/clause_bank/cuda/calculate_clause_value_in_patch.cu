6dc4072c9315e8c69b2d5914eff6123a7f7d2928
Ole-Christoffer Granmo <ole.granmo@uia.no>
2024-10-11 10:04:12 +0200

    Update

--- tmu/clause_bank/cuda/calculate_clause_value_in_patch.cu
+++ tmu/clause_bank/cuda/calculate_clause_value_in_patch.cu
@@ -96,6 +96,10 @@ extern "C"
                 if ((ta_state[(number_of_ta_chunks-1)*number_of_state_bits + number_of_state_bits - 1] & (X[node*number_of_ta_chunks + number_of_ta_chunks-1] | (!literal_active[number_of_ta_chunks-1])) & filter) != (ta_state[(number_of_ta_chunks-1)*number_of_state_bits + number_of_state_bits - 1] & filter)) {
                     clause_node_output &= ~(1 << node_pos);
                 }
+
+                if (node == 0) {
+                    printf("V: %d\n", clause_node_output & (1 << node_pos));
+                }
             }
 
             if (node_chunk == number_of_node_chunks - 1) {
